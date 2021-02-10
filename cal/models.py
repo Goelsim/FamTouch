649dc101c02c8167986a778c287f80a84330333c
Goelsim <simran.goel1998@gmail.com>
2021-02-11 00:39:20 +0530

    Initial commit

---
+++ cal/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    description = models.TextField()
